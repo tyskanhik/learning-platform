6ae333ae18997ff87ae94feceea77ff8152661fa
Arctic Fox <tyskanhik@mail.ru>
2025-10-23 22:03:03 +0300

    update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,31 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
-import { provideStore } from '@ngrx/store';
-import { reducers } from './core/store';
-import { provideEffects } from '@ngrx/effects';
-import { CourseEffects } from './core/store/course/course.effects';
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { UserState } from './core/store/user.state';
+import { CourseState } from './core/store/course.state';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(withFetch()), provideTransloco({
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideHttpClient(withFetch()), 
+    provideTransloco({
         config: {
             availableLangs: ['ru', 'en'],
             defaultLang: 'ru',
-            // Remove this option if your application doesn't support changing language in runtime.
             reRenderOnLangChange: true,
             prodMode: !isDevMode(),
         },
         loader: TranslocoHttpLoader
     }),
-    provideStore(reducers),
-    provideEffects([CourseEffects])
-]
+    provideStore(
+      [UserState, CourseState],
+      withNgxsReduxDevtoolsPlugin({
+        disabled: !isDevMode() 
+      })
+    )
+  ]
 };
